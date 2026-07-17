bed19196b754e5687e41ae4c0fd01c25f352529d
Ashokkumar <babajiashok8637@gmail.com>
2026-07-17 13:25:26 +0530

    socket-client

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,29 @@ import Login from "./pages/login.jsx";
 import Sign from "./pages/sign.jsx";
 import Chat from "./pages/chat.jsx";
 import { AuthProvider } from "./componets/authen.jsx";
+import { useEffect } from "react";
+import { socket } from "./socket.js";
 
 function App() {
+
+  useEffect(()=>{
+    socket.on("Connect" , ()=>{
+      console.log("Connected");
+
+      console.log(socket.id);
+    });
+
+    socket.on( "Welcome" , (msg) =>{
+      console.log(msg);
+    });
+
+    socket.emit("Hello" , "Hello Server");
+
+    return()=>{
+      socket.off("connect");
+      socket.off("welcome");
+    }
+},[]);
   return (
     <AuthProvider>
       <BrowserRouter>
